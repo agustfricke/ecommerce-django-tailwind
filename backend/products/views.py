a19c0d7d51a197c7839f430ab2b8d9725c4098fb
Agust Fricke <agustfricke@protonmail.com>
2022-12-27 19:26:54 -0300

    carrousel and pagination

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,6 +3,8 @@ from django.utils.text import slugify
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Q
+from django.core.paginator import Paginator
+
 
 from . models import Product, Category
 from . forms import ProductForm
@@ -26,8 +28,14 @@ def create(request):
     return render(request, 'products/create.html', {'form':form})
 
 def home(request):
-    products = Products.objects.all(request)
-    return render(request, 'products/home.html')
+    last = Product.objects.all()[0:3]
+    products = Product.objects.all()
+
+    paginator = Paginator(products, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'products/home.html', {'page_obj':page_obj, 'last':last})
 
 def product(request):
     return render(request, 'products/product.html')
